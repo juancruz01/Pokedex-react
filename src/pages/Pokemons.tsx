15a33b3cf3a5ea7d20667846b8bcd347d7489aba
juancruz01 <juancruzc278@gmail.com>
2025-09-18 02:10:13 -0300

    creacion de lista pokemon en el main y su estilo en modulo

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
+import { Link } from "react-router-dom";
+
+import BulbasaurPic from "../assets/bulbasaur.gif";
+import styles from "./pokemons.module.css"
+
 /*
 Esto va a funcionar como pagina principal
 */
@@ -15,7 +20,29 @@ const Pokemons = () =>{
         <>
             <Header query={query} setQuery={setQuery} />
             <main>
-                <h1>Pokemons</h1>
+                <nav>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={BulbasaurPic} alt="Bulbasaur" />
+                        <div className={styles.listItemText}>
+                            <span>Bulbasaur</span>
+                            <span>001</span>
+                        </div>
+                    </Link>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={BulbasaurPic} alt="Ivysaur" />
+                        <div className={styles.listItemText}>
+                            <span>Ivysaur</span>
+                            <span>002</span>
+                        </div>
+                    </Link>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={BulbasaurPic} alt="" />
+                        <div className={styles.listItemText}>
+                            <span>Venusaur</span>
+                            <span>003</span>
+                        </div>
+                    </Link>
+                </nav>
             </main>
             <Footer />
 
