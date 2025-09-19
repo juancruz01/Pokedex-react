913f323ae0ad5073fe88177e1f5191a75d0d88a4
juancruz01 <juancruzc278@gmail.com>
2025-09-18 21:13:12 -0300

    new folder utils - renderizado de todo el listado pokemon

---
+++ src/api/fetchPokemons.tsx
@@ -0,0 +1,28 @@
+import { Pokemon } from "../types/types";
+import { formatPokemonName } from "../utils/utils";
+
+export async function fetchPokemons(): Promise<Pokemon[]> {
+    const response = await fetch(
+        "https://unpkg.com/pokemons@1.1.0/pokemons.json"
+    )
+
+    if (!response.ok){
+        throw new Error("Failed to fetch pokemons")
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const pokemons = data.results.map((pokemon : any) => ({
+        name: pokemon.name,
+        id: pokemon.national_number,
+        imgSrc: `https://img.pokemondb.net/sprites/black-white/anim/normal/${formatPokemonName(pokemon.name)}.gif`,
+    }));
+
+    const uniquePokemons = pokemons.filter(
+        (pokemon: any, index: number) =>
+            pokemons.findIndex((other: any) => other.id === pokemon.id) === index
+    );
+    return uniquePokemons;
+};
+
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
 import { Link } from "react-router-dom";
 
 import BulbasaurPic from "../assets/bulbasaur.gif";
 import styles from "./pokemons.module.css"
+import { fetchPokemons } from "../api/fetchPokemons";
+import { Pokemon } from "../types/types";
 
 /*
 Esto va a funcionar como pagina principal
@@ -13,35 +15,34 @@ Esto va a funcionar como pagina principal
 const Pokemons = () =>{
 
     /* Es lo que que es consultal el nombre ingresado(query) por el input para obtener los datos del pokemon */
-    const [query, setQuery] = useState("BULBASUR");
+    const [query, setQuery] = useState("");
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+
+    useEffect(() => {
+        const fetchAllPokemons = async() => {
+            const allPokemons = await fetchPokemons();
+            console.log(allPokemons)
+            setPokemons(allPokemons);
+        };
+        fetchAllPokemons();
+    },[])
 
     return(
         <>
             <Header query={query} setQuery={setQuery} />
             <main>
                 <nav>
-                    <Link className={styles.listItem} to="/">
+                    {pokemons?.slice(0, 151).map((pokemon) => (
+                        <Link className={styles.listItem} to="/">
                         <img className={styles.listItemIcon} src={BulbasaurPic} alt="Bulbasaur" />
                         <div className={styles.listItemText}>
                             <span>Bulbasaur</span>
                             <span>001</span>
                         </div>
                     </Link>
-                    <Link className={styles.listItem} to="/">
-                        <img className={styles.listItemIcon} src={BulbasaurPic} alt="Ivysaur" />
-                        <div className={styles.listItemText}>
-                            <span>Ivysaur</span>
-                            <span>002</span>
-                        </div>
-                    </Link>
-                    <Link className={styles.listItem} to="/">
-                        <img className={styles.listItemIcon} src={BulbasaurPic} alt="" />
-                        <div className={styles.listItemText}>
-                            <span>Venusaur</span>
-                            <span>003</span>
-                        </div>
-                    </Link>
+                    ))};
+                    
                 </nav>
             </main>
             <Footer />
