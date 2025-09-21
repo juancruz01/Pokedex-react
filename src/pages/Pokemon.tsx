d75fd3f8910733a6b472c689ee45c7d5d8c8c69d
juancruz01 <juancruzc278@gmail.com>
2025-09-21 16:24:59 -0300

    avances en la page pokemon  estilos y retoques finales

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,27 +1,50 @@
 import { useNavigate, useParams } from "react-router-dom";
 import pokeballImg from "../assets/pokeball.png";
 import Footer from "../components/Footer"; 
-import BulbasaurImg from "../assets/bulbasaur.gif"
+import styles from "./pokemon.module.css";
+import { useEffect, useState } from "react";
+import { PokemonDetails } from "../types/types";
+import { fetchPokemon } from "../api/fetchPokemon";
+import LoadingScreen from "../components/LoadingScreen";
+import { waitFor } from "../utils/utils";
 
 const Pokemon = () =>{
 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [pokemon, setPokemon] = useState<PokemonDetails>(); 
     const {name} = useParams();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        async function getPokemon() {
+            setIsLoading(true);
+            await waitFor(300);
+            const fetchedPokemon = await fetchPokemon(name as string);
+            setPokemon(fetchedPokemon);
+            setIsLoading(false);
+        }
+        getPokemon()
+
+    }, [name]);
+
+    if (isLoading || !pokemon) return <LoadingScreen />
+
     return <>
-            <button onClick= {() => navigate(-1)}>
-                <img src={pokeballImg} alt="pokeball" /> Go back
+            <button className={styles.pokeballButton} onClick= {() => navigate(-1)}>
+                <img className={styles.pokeballImg} src={pokeballImg} alt="pokeball" /> Go back
             </button>
-            <div>
-                <main>
-                    <div>{name?.toUpperCase()}</div>
-                    <div>Nr. 001</div>
+            <div className={styles.pokemon}>
+                <main className={styles.pokemonInfo}>
+                    <div className={styles.pokemonTitle} >{pokemon?.name
+                    ?.toUpperCase()}</div>
+                    <div>Nr. {pokemon?.id}</div>
                     <div>
-                        <img src={BulbasaurImg} alt="" />
+                        <img className={styles.pokemonInfoImg} src={pokemon?.imgSrc} alt={pokemon?.name} />
                     </div>
-                    <div>HP: 80</div>
-                    <div>Attack: 50</div>
-                    <div>Defense: 30</div>
+                    <div>HP: {pokemon?.hp}</div>
+                    <div>Attack: {pokemon?.attack}</div>
+                    <div>Defense: {pokemon?.defense}</div>
                 </main>
             </div>
             <Footer />
